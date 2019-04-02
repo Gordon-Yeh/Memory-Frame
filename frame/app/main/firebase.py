81d3bc942d62dd65d7d8857a9a9883a5a89f4a64
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-04-01 22:53:09 -0700

    firebase init integration

--- frame/app/main/firebase.py
+++ frame/app/main/firebase.py
@@ -10,8 +10,9 @@ class Firebase:
 
     def sign_in(self, email, psw):
         self.user = self.auth.sign_in_with_email_and_password(email, psw)
+        return self.user
     
-    def filter(self, key, val):
+    def filter(self, key, val, user):
         """query the database: select url from user_photos where key = val
         Args:
             key: name of the field in a database entry 
@@ -23,33 +24,17 @@ class Firebase:
         eg. user = firebase.sign_in('user0@gmail.com', '123123')
             empty_name = firebase.filter('name', '',user)
         """
-        query = self.database.child(self.user.get('localId')).order_by_child(key).equal_to(val).get(token = self.user.get('idToken'))
-        if not query.pyres:
-            return []
-        all_val = query.val()
+        all_val = self.database.child(user.get('localId')).order_by_child(key).equal_to(val).get(token = user.get('idToken')).val()
         result = []
         for val in all_val:
-          result.append(self.database.child(self.user.get('localId')).child(val).get(token = self.user.get('idToken')).val().get("imageUrl"))
+          result.append(self.database.child(user.get('localId')).child(val).get(token = user.get('idToken')).val().get("imageUrl"))
         return result
 
-    def show_all(self):
+    def show_all(self, user):
         """ All pictures under the account of the given user
         Returns an array of URLs to the pictures
         """
-        query = self.database.child(self.user.get('localId')).get(token = self.user.get('idToken'))
-        if not query.pyres:
-            return []
-        all_val = query.val()
-        result = []
-        for val in all_val:
-            result.append(self.database.child(self.user.get('localId')).child(val).get(token = self.user.get('idToken')).val().get("imageUrl"))
-        return result
-
-    def tag_search(self, tag, user):
-        query = self.database.child(user.get('localId')).order_by_child('caption').start_at(tag).end_at(tag+'\uf8ff').get(token = user.get('idToken'))
-        if not query.pyres:
-            return []
-        all_val = query.val()
+        all_val = self.database.child(user.get('localId')).get(token = user.get('idToken')).val()
         result = []
         for val in all_val:
             result.append(self.database.child(user.get('localId')).child(val).get(token = user.get('idToken')).val().get("imageUrl"))
