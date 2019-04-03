83870c06104b230a44f1665af935b0828b5148d5
richardyjtian <richardyjtian@hotmail.com>
2019-04-02 18:10:48 -0700

    Bug fixes

--- PhotoFrame/app/src/main/java/io/github/richardyjtian/photoframe/RegisterActivity.java
+++ PhotoFrame/app/src/main/java/io/github/richardyjtian/photoframe/RegisterActivity.java
@@ -84,7 +84,7 @@ public class RegisterActivity extends AppCompatActivity {
             });
 
             email.addTextChangedListener(new JumpText((email), user));
-            user.addTextChangedListener(new JumpText((user), pwd));
+            user.addTextChangedListener(new JumpText((user), psw));
 
         }
 
